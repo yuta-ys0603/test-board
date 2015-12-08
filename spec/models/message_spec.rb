5272e2a1d120096ebb4794a1745c6c23235901aa
yuta-ys0603 <yu06ta03@gmail.com>
2015-12-08 20:24:07 +0900

    wrote test code

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
  describe Message do
-    it 'does not allow empty title'
-    it 'does not allow empty body'
-    it 'is valid with title and body'
+
+    it 'does not allow empty title' do
+        expect(Message.new(title: '')).to have(1).errors_on(:title)
+    end
+    it 'does not allow empty body' do
+      expect(Message.new(body: '')).to have(1).errors_on(:body)
+    end
+    it 'is valid with title and body' do
+      expect(Message.new(title: 'hoge', body: 'fuga')).to be_valid
+    end
  end
