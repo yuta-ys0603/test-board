e019857f8924c7e05b640611e8fa654a94c9204b
yuta-ys0603 <yu06ta03@gmail.com>
2015-12-17 19:15:20 +0900

    create FactoryGirl

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
  describe Message do
 
     it 'does not allow empty title' do
-        expect(Message.new(title: '')).to have(1).errors_on(:title)
+        expect(FactoryGirl.build(:message, title: '')).to have(1).errors_on(:title)
     end
     it 'does not allow empty body' do
-      expect(Message.new(body: '')).to have(1).errors_on(:body)
+      expect(FactoryGirl.build(:message, body: '')).to have(1).errors_on(:body)
     end
     it 'is valid with title and body' do
-      expect(Message.new(title: 'hoge', body: 'fuga')).to be_valid
+      expect(FactoryGirl.build(:message)).to be_valid
     end
  end
